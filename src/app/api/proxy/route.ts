07efc099456094289f7d47e33594505ba00822d5
AnshuPathak-88825 <shubhanshupathak88825@gmail.com>
2024-02-21 22:02:24 +0530

    created PUT proxy

--- src/app/api/proxy/route.ts
+++ src/app/api/proxy/route.ts
@@ -65,8 +65,8 @@ export async function POST(req: NextRequest, res: NextResponse) {
     else if (method === "POST") {
         try {
             const option = urlData.option;
-            const apidata=await axios.post(url,option,{
-                withCredentials:true
+            const apidata = await axios.post(url, option, {
+                withCredentials: true
             })
             const Header_cookie = apidata.headers["set-cookie"];
             const data = apidata.data
@@ -121,7 +121,66 @@ export async function POST(req: NextRequest, res: NextResponse) {
             return NextResponse.json({ "error": "An error occurred while parsing POST data" });
         }
     }
+    else if (method === "PUT") {
+        try {
+            const option = urlData.option;
+            const apidata = await axios.put(url, option, {
+                withCredentials: true
+            })
+            const Header_cookie = apidata.headers["set-cookie"];
+            const data = apidata.data;
+            const response = NextResponse.json({ data }, { status: 200 })
+            Header_cookie?.map(biscuit => {
+                const cream = biscuit.split(";");
+                const [key, value] = cream[0].split("=");
 
+                class OptionsClass {
+                    constructor(
+                        public secure: boolean = false,
+                        public httpOnly: boolean = false,
+                        public expires: number | Date | undefined = undefined // Adjust the type to be compatible
+                    ) { }
+
+                    [key: string]: any;
+                }
+                let Option: OptionsClass = new OptionsClass();
+
+                for (const element of cream.slice(1)) {
+                    const [key, value] = element.split("=");
+                    if (key.trim() === 'expires') {
+                        const date = new Date(value);
+                        Option[key.trim()] = date;
+                    } else {
+                        Option[key.trim()] = (value === undefined ? true : false);
+                    }
+                }
+                const { expires, Secure, HttpOnly } = Option;
+                const NewOption = new OptionsClass;
+                NewOption.expires = Option.expires;
+                NewOption.httpOnly = Option.HttpOnly;
+                NewOption.secure = Option.Secure;
+                response.cookies.set(key, value, NewOption);
+            });
+            if (apidata.headers !== undefined && apidata.headers !== null) {
+                Object.keys(apidata.headers).forEach(key => {
+                    const value = apidata.headers[key];
+                    // console.log(`${key}: ${value}`);
+
+                    if (value !== undefined && value !== null) {
+
+                        response.headers.set(key, value.toString());
+                    }
+                });
+            }
+            return response;
+
+
+        } catch (error) {
+            console.error('Error parsing PUT data:', error);
+            return NextResponse.json({ "error": "An error occurred while parsing POST data" });
+        }
+
+    }
 
 
 }
