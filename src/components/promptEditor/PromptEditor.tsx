acd9db7e1acf6b7481254037a36960e90b148a0c
AnshuPathak-88825 <shubhanshupathak88825@gmail.com>
2024-02-28 07:36:48 +0530

    added toast and json copy feature

--- src/components/promptEditor/PromptEditor.tsx
+++ src/components/promptEditor/PromptEditor.tsx
@@ -4,10 +4,16 @@ import { Textarea } from "@/components/ui/textarea";
 import { Button } from "../ui/button";
 import axios from "axios";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import { ReloadIcon } from "@radix-ui/react-icons"
+import { useToast } from "@/components/ui/use-toast"
+
 type Props = {};
 
 const PromptEditor = (props: Props) => {
+    const { toast } = useToast()
+    const [textTocopy, settextTocopy] = useState("");
     const [loading, setLoading] = useState(false);
     const [data, setData]: any = useState("");
     const [error, setError] = useState(null);
@@ -24,14 +30,18 @@ const PromptEditor = (props: Props) => {
 
     async function run() {
         try {
+            setData("");
             setLoading(true)
             const model = genAI.getGenerativeModel({ model: "gemini-pro" });
             const result = await model.generateContent(`${promptvalue.current} generate json random value of givens schema `);
             const response = await result.response;
             const text = response.text();
-            // console.log(text);
             setData(text);
-            console.log(data);
+            const startIndex = text.indexOf('{');
+            const endIndex = text.lastIndexOf('}') + 1;
+
+            const jsonData = text.substring(startIndex, endIndex);
+            setData(jsonData);
             setLoading(false);
         } catch (error) {
             setData("Try again");
@@ -61,6 +71,20 @@ const PromptEditor = (props: Props) => {
         }
     }, { timestamps: true });
     `;
+    const copyhandler = async () => {
+        try {
+            navigator.clipboard.writeText(data);
+            toast({
+                variant: "success",
+                title: "Text Copied",
+                description: "The text has been successfully copied to the clipboard.",
+            })
+
+
+        } catch (error) {
+
+        }
+    }
 
     return (
         <div className="grid w-full  gap-2 p-1">
@@ -81,7 +105,19 @@ const PromptEditor = (props: Props) => {
                 <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
                 Please wait
             </Button>}
-            {!loading && <div>{data}</div>}
+            {!loading && <div>
+                <SyntaxHighlighter language="javascript" style={docco}>
+                    {data}
+                </SyntaxHighlighter>
+            </div>
+            }
+            {data && <Button
+                onClick={copyhandler}
+
+            >
+                copy
+            </Button>}
+
         </div>
     );
 };
